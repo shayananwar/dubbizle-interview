48a4b6e5691cb075c9f502ec898695a05f87feb9
Shayan <muhammad.shayan@arpatech.com>
2021-03-05 12:11:03 +0500

    work initiated

--- src/components/Gist.js
+++ src/components/Gist.js
@@ -1,3 +1,68 @@
-const Gist = ({ gist }) => <>
+import styled from 'styled-components';
+import Octicon from 'react-octicon';
+
+function Gist(props) {
+	console.log(props.data)
+	return (
+	    <Wrapper>
+		    <div style={userSection}>
+		    	<div style={userProfile}>
+		    		<img src={props.data.owner.avatar_url} style={userPic} />
+		    		<a href={props.data.owner.html_url} style={userName}> {props.data.owner.login} </a>
+		    	</div>
+
+		    	<div>
+		    		<a target="_blank" href={props.data.url} style={userName}><Octicon name="code" /> {Object.keys(props.data.files).length} Files</a>
+
+		    		<a target="_blank" href={props.data.forks_url} style={userName}><Octicon name="fork" /> Forks</a>
+
+		    		<a target="_blank" href={props.data.comments_url} style={userName}><Octicon name="comment" /> Comments</a>
+
+		    		<a target="_blank" href={props.data.owner.starred_url} style={userName}><Octicon name="star" /> Stars</a>
+		    	</div>
+		    </div>
+
+		    <div>
+
+		    </div>
+
+		    <div>
+
+		    </div>
+
+			<div>
+
+		    </div>		    
+	    </Wrapper>
+	);
+}
+
+const Wrapper = styled.div`
+  width: 100%;
+  margin: auto;
+`;
+
+const userSection = {
+	display: "flex",
+	justifyContent: "space-between"
+};
+
+const userProfile = {
+  width: 'auto',
+  display: 'flex'
+};
+
+const userPic = {
+	width: '50px',
+	borderRadius: '50%'
+};
+
+const userName = {
+	alignSelf: 'center',
+	marginLeft: '10px',
+	fontSize: '16px',
+	color: '#0a52e3',
+	textDecoration: 'none'
+};
 
 export default Gist
--- src/components/GistList.js
+++ src/components/GistList.js
@@ -1,4 +1,52 @@
-import React, { useEffect, useState } from 'react'
-const GistList = () => <></>
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import {getPublicGists, getGistForUser} from '../services/gistService';
+import Gist from "./Gist";
+
+function GistList() {
+	// Declare a new state variable, which we'll call "gists"  
+	const [gists, setGists] = useState([]);
+
+	//call service and fetch data
+	async function fetchGists(search, cb) {
+		try {
+			let response = [];
+			
+			if(search.length > 0)
+				response = await getGistForUser(search);
+			else
+				response = await getPublicGists();
+
+			if(typeof response.data !== undefined)
+				cb(response.data);
+		} catch(error) {
+			// TODO: handle error cases
+			cb([]);
+		}
+	}
+
+	// Similar to componentDidMount
+	useEffect(() => {    
+		fetchGists("", function(gistsResponse) {
+			setGists(gistsResponse);
+		});
+	}, []);
+
+  return (
+    <Wrapper>
+	    {
+	    	gists.map(function(gist, i){
+	        	return <Gist data={gist} key={i} />
+	    	})
+	    }
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  width: 40%;
+  margin: auto;
+  padding: 10px 0px;
+`;
 
 export default GistList
